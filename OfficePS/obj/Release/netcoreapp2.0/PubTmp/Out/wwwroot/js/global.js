19519495faf2fb5e938291d4a736c461c4e22bd7
edmarssk <edmar.santos1986@gmail.com>
2018-09-28 14:34:47 -0300

    Primeiro

---
+++ OfficePS/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/global.js
@@ -0,0 +1,14 @@
+﻿var divPrinc = document.getElementById("divPrincipal");
+
+var botao = document.getElementById("botaoAbrirFechar");
+
+botao.addEventListener("click", AbrirFecharDiv);
+
+function AbrirFecharDiv() {
+    if (divPrinc.hidden == true) {
+        divPrinc.hidden = false;
+    }
+    else {
+        divPrinc.hidden = true;
+    }
+}
